926c8c8a9f6b3b223f081301eee63709bdc6c32a
Gergo Nagy <guzmop@gmail.com>
2019-02-24 11:53:11 +0000

    Updates

---
+++ precache-manifest.21491c3ad7fdc31ebf13662083b688e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a4506a8f66ee909b9df",
+    "url": "/contact-manger-react/static/js/runtime~main.8a4506a8.js"
+  },
+  {
+    "revision": "a2ea5e955b687709a3fd",
+    "url": "/contact-manger-react/static/js/main.a2ea5e95.chunk.js"
+  },
+  {
+    "revision": "0aef252876c4bd6ba29c",
+    "url": "/contact-manger-react/static/js/1.0aef2528.chunk.js"
+  },
+  {
+    "revision": "a2ea5e955b687709a3fd",
+    "url": "/contact-manger-react/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "0aef252876c4bd6ba29c",
+    "url": "/contact-manger-react/static/css/1.54ba07f6.chunk.css"
+  },
+  {
+    "revision": "c83fd1114fc34b5a6ad193a85e810948",
+    "url": "/contact-manger-react/index.html"
+  }
+];
