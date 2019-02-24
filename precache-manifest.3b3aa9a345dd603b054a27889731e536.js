3845ba5de78c51e95eac57dc9671a95220558edc
Gergo Nagy <guzmop@gmail.com>
2019-02-24 11:55:03 +0000

    Updates

--- precache-manifest.3b3aa9a345dd603b054a27889731e536.js
+++ precache-manifest.3b3aa9a345dd603b054a27889731e536.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/contact-manger-react/static/js/runtime~main.8a4506a8.js"
   },
   {
-    "revision": "a2ea5e955b687709a3fd",
-    "url": "/contact-manger-react/static/js/main.a2ea5e95.chunk.js"
+    "revision": "c56010a6e790032c3b82",
+    "url": "/contact-manger-react/static/js/main.c56010a6.chunk.js"
   },
   {
-    "revision": "0aef252876c4bd6ba29c",
-    "url": "/contact-manger-react/static/js/1.0aef2528.chunk.js"
+    "revision": "5c7e1814010c4a903949",
+    "url": "/contact-manger-react/static/js/1.5c7e1814.chunk.js"
   },
   {
-    "revision": "a2ea5e955b687709a3fd",
+    "revision": "c56010a6e790032c3b82",
     "url": "/contact-manger-react/static/css/main.da3b6268.chunk.css"
   },
   {
-    "revision": "0aef252876c4bd6ba29c",
+    "revision": "5c7e1814010c4a903949",
     "url": "/contact-manger-react/static/css/1.54ba07f6.chunk.css"
   },
   {
-    "revision": "c83fd1114fc34b5a6ad193a85e810948",
+    "revision": "662c9510c364b34b9397cebbf04b60b9",
     "url": "/contact-manger-react/index.html"
   }
 ];
